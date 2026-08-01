d4ddb82565fc339f00e8f391f86c149e32eae9f9
eghrhegpe <134900655+eghrhegpe@users.noreply.github.com>
2026-08-01 12:57:39 +0800

    网页测试

--- docs/guide/.vitepress/theme/index.ts
+++ docs/guide/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
+import './style.css'
 
 // 扩展默认主题：仅替换 Layout 以注入侧栏常驻逻辑，其余全部沿用默认主题。
 export default {
